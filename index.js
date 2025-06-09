8d771dd94fb60f958259669d88e54a26d7024d91
sharon caroline <you@example.com>
2025-06-09 12:44:50 +0300

    Unit-3-JS-Looping

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return messages;
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
+countDown(10);
